05a814fead76f7ea29cdc2c9640fee19444789d8
John Lowther <jw.lowther@hotmail.co.uk>
2022-12-31 14:23:20 +0000

    Remove server project

--- server/src/setup/config.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-
-export default {
-  dbConnection: process.env.DB_CONNECTION ?? '',
-  microsoftJwksUri: process.env.MICROSOFT_JWKS_URI ?? '',
-  sessionSecret: process.env.SESSION_SECRET ?? '',
-};
--- server/src/setup/db.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-
-// Mongoose re-exported for utility project
-export default mongoose;
-export * from 'mongoose';
