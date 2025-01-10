efe13f673b21aee16efafe9580db0f77a57e8416
John Lowther <jw.lowther@hotmail.co.uk>
2025-01-10 23:05:45 +0000

    Remove templates folder

--- templates/eslint.config.js
+++
@@ -1,7 +0,0 @@
-import js from "@eslint/js";
-import tseslint from 'typescript-eslint';
-
-export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-];
--- templates/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { templateRoots } from "./collections";
